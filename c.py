b9611b80f4197c16b8bc757088bb56f7d34f1b12
miguelhpreuss <45389687+miguelhpreuss@users.noreply.github.com>
2018-12-02 22:24:39 -0200

    Add files via upload

---
+++ c.py
@@ -0,0 +1,33 @@
+from pyautogui import*   
+'''
+click(x,y) #clicar em uma coordenada
+click() #click na coordenada atual
+click(button='right') #
+position(x,y) #mostra coordenada atual do mouse
+size() #mostra tamanho do monitor
+moveTo(x,y) #mover mouse até coordenada
+moveTo(x,y,tempo) #com segundos
+moveRel(x,y) #mover mouse em distância
+dragTo(x,y,tempo) #mover mouse segurando botao middle left right
+dragRel(x,y,tempo) #mover mouse em distância segurando botao
+click(clicks=2)
+click(clicks=2, interval=0.25)
+click(button='right',clicks=2,interval=0.25)
+doubleClick()
+tripleClick()
+rightClick()
+leftClick()
+mouseDown()
+mouseUp()
+scroll()
+vscroll() #vertical
+press('enter' ou 'esc' ou 'f1')#pode ser em lista
+hotkey() #pressionar e desprecionar ao mesmo tempo
+keyDown()
+keyUp()
+'''
+'''
+click(610, 737)
+typewrite("ola brena, como vai?", interval=0.1)
+click(1333, 735)
+'''
